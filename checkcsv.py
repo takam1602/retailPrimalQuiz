f5b2df9a25fa76b9ddb6ba8aca93d36b9a83473d
Tatsuki Kamata <kmtttk1602@gmail.com>
2024-05-28 17:55:15 +0900

    salable

--- checkcsv.py
+++ checkcsv.py
@@ -2,8 +2,12 @@ import pandas as pd
 
 # CSVファイルの内容を読み込み表示する
 file_path = './retailIndex.csv'
+sfile_path = './salableIndex.csv'
 csv_data = pd.read_csv(file_path)
+scsv_data = pd.read_csv(sfile_path)
 print(csv_data)
+print(scsv_data)
 
 # CSVファイルをUTF-8エンコーディングで再保存
 csv_data.to_csv('retailIndex_utf8.csv', index=False, encoding='utf-8')
+scsv_data.to_csv('salableIndex_utf8.csv', index=False, encoding='utf-8')
